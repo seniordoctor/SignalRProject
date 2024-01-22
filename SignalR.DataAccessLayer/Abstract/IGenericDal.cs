9cda74c59a9c2bd5539c317decfae21dcccb6140
Recep Mert <doctorprivate@outlook.com>
2024-01-22 23:24:03 +0300

    Add project files.

---
+++ SignalR.DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T : class         // T degeri mutlaka bir class olmali. Disariden nesne, method giremeyecek
+    {
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+        T GetById(int id);
+        List<T> GetListAll();
+    }
+}
---
+++ SignalR.DataAccessLayer/EntityFramework/EfSocialMediaDal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SignalR.DataAccessLayer.Abstract;
+using SignalR.DataAccessLayer.Concreate;
+using SignalR.DataAccessLayer.Repositories;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalR.DataAccessLayer.EntityFramework
+{
+    public class EfSocialMediaDal : GenericRepository<SocialMedia>, ISocialMediaDal
+    {
+        public EfSocialMediaDal(SignalRContext context) : base(context)
+        {
+        }
+    }
+}
