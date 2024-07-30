43ba918a7a25757cc49ed1b207e119678fa266ba
Doctor <75563269+seniordoctor@users.noreply.github.com>
2024-07-30 22:13:42 +0300

    Auto Mapper Added & About API Added

---
+++ SignalRApi/Mapping/AboutMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using SignalR.DtoLayer.AboutDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Mapping;
+
+public class AboutMapping : Profile
+{
+    public AboutMapping()
+    {
+        // About sınıfı ve ResultAboutDto sınıfı arasında mapping işlemi yapılır.
+        CreateMap<About, ResultAboutDto>().ReverseMap(); // ReverseMap() metodu ile iki sınıf arasında çift yönlü mapping işlemi yapılır.
+        CreateMap<About, CreateAboutDto>().ReverseMap();
+        CreateMap<About, GetAboutDto>().ReverseMap();
+        CreateMap<About, UpdateAboutDto>().ReverseMap();
+    }
+}
