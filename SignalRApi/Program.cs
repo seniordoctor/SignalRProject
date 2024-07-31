455c57c5d1d67bd32fd0e3a2cb0b0a7bfc4a00a9
Doctor <75563269+seniordoctor@users.noreply.github.com>
2024-07-31 22:28:05 +0300

    Booking API & Category API Add with Mapper

--- SignalRApi/Program.cs
+++ SignalRApi/Program.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using SignalR.BusinessLayer.Abstract;
 using SignalR.BusinessLayer.Concrete;
 using SignalR.DataAccessLayer.Abstract;
@@ -7,9 +8,16 @@ using SignalR.DataAccessLayer.EntityFramework;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<SignalRContext>();
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
 builder.Services.AddScoped<IAboutService, AboutManager>();
 builder.Services.AddScoped<IAboutDal, EfAboutDal>();
+
+builder.Services.AddScoped<IBookingService, BookingManager>();
+builder.Services.AddScoped<IBookingDal, EfBookingDal>();
+
+builder.Services.AddScoped<ICategoryService, CategoryManager>();
+builder.Services.AddScoped<ICategoryDal, EfCategoryDal>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
